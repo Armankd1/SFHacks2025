7a008b6120889045af63db3a9abf9f0c2a8d0e84
Armankd1 <armandaghbashyan@gmail.com>
2025-04-05 20:40:10 -0700

    dynamic webpage

---
+++ public/data/exercise.js
@@ -0,0 +1,18 @@
+let exerciseData = [
+    {
+        exerciseName: "JumpingJack",
+        exerciseHeader: "Jumping Jack",
+        exerciseText: "full body cardio – medium"
+     },
+    {
+        exerciseName: "Squat",
+        exerciseHeader: "Squat",
+        exerciseText: "legs and core – medium"   
+ },
+    {
+        exerciseName: "HighKneeRaises",
+        exerciseHeader: "High Knee Raises",
+        exerciseText: "lower body strength – easy"
+    }
+];
+export default exerciseData;
