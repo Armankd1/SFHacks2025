681db15e72a75978ea501a1e4ad741fa1c3ce5a1
Armankd1 <armandaghbashyan@gmail.com>
2025-04-05 21:26:50 -0700

    dynamic explanations and audio

--- public/data/exercise.js
+++ public/data/exercise.js
@@ -2,17 +2,20 @@ let exerciseData = [
     {
         exerciseName: "JumpingJack",
         exerciseHeader: "Jumping Jack",
-        exerciseText: "full body cardio – medium"
+        exerciseText: "full body cardio – medium",
+        audioSource: "../sounds/jumpingJack.mp3"
      },
     {
         exerciseName: "Squat",
         exerciseHeader: "Squat",
-        exerciseText: "legs and core – medium"   
+        exerciseText: "legs and core – medium",
+        audioSource: "../sounds/squat.mp3"
  },
     {
         exerciseName: "HighKneeRaises",
         exerciseHeader: "High Knee Raises",
-        exerciseText: "lower body strength – easy"
+        exerciseText: "lower body strength – easy",
+        audioSource: "../sounds/kneeRaise.mp3"
     }
 ];
 export default exerciseData;
