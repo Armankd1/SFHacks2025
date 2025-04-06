0995ad666ad3521207e759ffffc5b23c9abd9e86
Armankd1 <armandaghbashyan@gmail.com>
2025-04-05 23:46:13 -0700

    minor changes2

--- public/data/exercise.js
+++ public/data/exercise.js
@@ -2,38 +2,23 @@ let exerciseData = [
     {
         exerciseName: "JumpingJack",
         exerciseHeader: "Jumping Jack",
-<<<<<<< HEAD
         exerciseText: "Full Body Cardio – Medium",
-        audioSource: "../sounds/jumpingJack.mp3"
-=======
-        exerciseText: "full body cardio – medium",
         audioSource: "../sounds/jumpingJack.mp3",
         modelPage: "../modelJJ.html"
->>>>>>> 54a1ccb42dee3e8ed121fb09d3155f61f2f0ca09
      },
     {
         exerciseName: "Squat",
         exerciseHeader: "Squat",
-<<<<<<< HEAD
         exerciseText: "Legs And Core – Medium",
-        audioSource: "../sounds/squat.mp3"
-=======
-        exerciseText: "legs and core – medium",
         audioSource: "../sounds/squat.mp3",
         modelPage: "../modelSQ.html"
->>>>>>> 54a1ccb42dee3e8ed121fb09d3155f61f2f0ca09
  },
     {
         exerciseName: "HighKneeRaises",
         exerciseHeader: "High Knee Raises",
-<<<<<<< HEAD
         exerciseText: "Lower Body Strength – Easy",
-        audioSource: "../sounds/kneeRaise.mp3"
-=======
-        exerciseText: "lower body strength – easy",
         audioSource: "../sounds/kneeRaise.mp3",
         modelPage: "../modelLR.html"
->>>>>>> 54a1ccb42dee3e8ed121fb09d3155f61f2f0ca09
     }
 ];
 export default exerciseData;
